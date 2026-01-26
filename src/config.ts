5b05b90de6a002c7269944ca2973370901699e3f
YumeHinata <yume3099729829@gmail.com>
2026-01-27 02:12:27 +0800

    修改博客主题配置文件

--- src/config.ts
+++ src/config.ts
@@ -17,7 +17,7 @@ export const siteConfig: SiteConfig = {
 	},
 	banner: {
 		enable: true,
-		src: "assets/images/demo-banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+		src: "src/assets/images/121506026_p0.webp", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
 			enable: true, // Display the credit text of the banner image
@@ -45,17 +45,21 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.Archive,
 		LinkPreset.About,
 		{
-			name: "GitHub",
-			url: "https://github.com/saicaca/fuwari", // Internal links should not include the base path, as it is automatically added
-			external: true, // Show an external link icon and will open in a new tab
+			name: "随机图API",
+			url: "https://rdimg.yumehinata.com/",
+			external: true,
+		}, {
+			name: "战术棋",
+			url: "/chess",
+			external: true,
 		},
 	],
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-	name: "Lorem Ipsum",
-	bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	avatar: "src/assets/images/72181756.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	name: "YumeHinata",
+	bio: "幻梦是个外行",
 	links: [
 		{
 			name: "Twitter",
