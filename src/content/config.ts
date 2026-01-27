6311f57a614ec15771a93f013fa9c1eb1088587d
YumeHinata <yume3099729829@gmail.com>
2026-01-27 12:32:23 +0800

    修改config.ts中published，添加pubDate

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,7 +3,8 @@ import { defineCollection, z } from "astro:content";
 const postsCollection = defineCollection({
 	schema: z.object({
 		title: z.string(),
-		published: z.date(),
+		published: z.boolean().default(true),   // ✅ 发布状态
+		pubDate: z.coerce.date(),
 		updated: z.date().optional(),
 		draft: z.boolean().optional().default(false),
 		description: z.string().optional().default(""),
